ea80d3835fff5840b77c1389ddac4bff64969d38
mdaleardc <ismailaliamot@gmail.com>
2024-11-12 18:25:12 +0600

    Link to NavLink updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 
 
@@ -8,24 +8,24 @@ const Navbar = () => {
   return (
     <div className="w-[100%] h-[60px] bg-[black] text-[#fffffe] fixed top-0 left-0 py-[5px]">
     <div className="grid grid-cols-6 gap-x-[4px] h-full w-full justify-center content-center">
-    <Link to="/">
+    <NavLink to="/">
     <button className="rounded bg-blue-600 w-full">Logo</button>
-    </Link>
-    <Link to="/home">
+    </NavLink>
+    <NavLink to="/home">
     <button className="rounded bg-blue-600 w-full">Home</button>
-    </Link>
-    <Link to="/hero">
+    </NavLink>
+    <NavLink to="/hero">
     <button className="rounded bg-blue-600 w-full">Hero</button>
-    </Link>
-    <Link to="/form">
+    </NavLink>
+    <NavLink to="/form">
     <button className="rounded bg-blue-600 w-full">Form</button>
-    </Link>
-    <Link to="/about">
+    </NavLink>
+    <NavLink to="/about">
     <button className="rounded bg-blue-600 w-full">About</button>
-    </Link>
-    <Link to="/footer">
+    </NavLink>
+    <NavLink to="/footer">
     <button className="rounded bg-blue-600 w-full">Footer</button>
-    </Link>
+    </NavLink>
     </div>
     </div>
     )
