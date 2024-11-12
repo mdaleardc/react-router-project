bbfb4534d344ec45a765aa8f7124edac198ccc03
mdaleardc <ismailaliamot@gmail.com>
2024-11-12 17:52:04 +0600

    Updated functional navigation

---
+++ src/Components/AllComp.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Home from "./Home";
+import Hero from "./Hero";
+import Form from "./Form";
+import About from "./About";
+import Footer from "./Footer";
+
+const AllComp = () => {
+  
+  return (
+    <>
+    <Home />
+    <Hero />
+    <Form />
+    <About />
+    <Footer />
+    
+    </>
+    )
+}
+
+export default AllComp;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,31 @@
 import React from "react"
+import { Link } from "react-router-dom";
+
 
 
 const Navbar = () => {
   
   return (
-    <div className="w-[100%] h-[60px] bg-[black] text-[#fffffe]">
-    <div className="grid grid-cols-5">
-    <button className="rounded">Logo</button>
-    <button className="rounded">Home</button>
-    <button className="rounded">Hero</button>
-    <button className="rounded">Form</button>
-    <button className="rounded">About</button>
-    <button className="rounded">Footer</button>
+    <div className="w-[100%] h-[60px] bg-[black] text-[#fffffe] fixed top-0 left-0 py-[5px]">
+    <div className="grid grid-cols-6 gap-x-[4px] h-full w-full justify-center content-center">
+    <Link to="/">
+    <button className="rounded bg-blue-600 w-full">Logo</button>
+    </Link>
+    <Link to="/home">
+    <button className="rounded bg-blue-600 w-full">Home</button>
+    </Link>
+    <Link to="/hero">
+    <button className="rounded bg-blue-600 w-full">Hero</button>
+    </Link>
+    <Link to="/form">
+    <button className="rounded bg-blue-600 w-full">Form</button>
+    </Link>
+    <Link to="/about">
+    <button className="rounded bg-blue-600 w-full">About</button>
+    </Link>
+    <Link to="/footer">
+    <button className="rounded bg-blue-600 w-full">Footer</button>
+    </Link>
     </div>
     </div>
     )
